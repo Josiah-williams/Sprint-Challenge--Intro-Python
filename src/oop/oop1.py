f9f4ab4128f197114bd208541c03fa0e9578cc5e
josiah <josiahdamiwilliams@gmail.com>
2020-06-05 10:05:10 +0100

    oop 1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -17,3 +17,18 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+class vehicle():
+    pass
+class FlightVehicle(vehicle):
+    pass
+class Airplane(FlightVehicle):
+    pass
+class Starship(FlightVehicle):
+    pass
+class GroundVehicle(vehicle):
+    pass
+class Car(GroundVehicle):
+    pass
+class Motorcycle(GroundVehicle):
+    pass
+
